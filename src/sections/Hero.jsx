23bf4590990f365cb3a1fe55be95c6e4c530b085
Mahmedk77 <mak.212601@gmail.com>
2025-08-25 17:23:13 +0500

    final push

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
       w-full max-xl:padding-x pt-28 ">
         <p className="text-xl font-montserrat
         text-coral-red">Our Summer Collection</p>
-        <h1 className="mt-10 font-palanquin text-8xl font-bold max-sm:text-[64px]
+        <h1 className="mt-10 font-palanquin text-8xl font-bold max-sm:text-[72px]
          max-sm:leading-[72px]">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-1 lg:z-10 pr-10">The New Arrival</span>
           <br />  
